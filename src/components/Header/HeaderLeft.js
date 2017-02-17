0188186f7363295d1463809ee99569b761ec10fa
saurabh297 <saurabh91verma@gmail.com>
2017-02-17 16:46:26 +0530

    addsaurabh

---
+++ src/components/Header/HeaderLeft.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+class HeaderLeft extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header/>
+      </div>
+    );
+  }
+}
+export default HeaderLeft;
---
+++ src/components/Header/HeaderRight.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+class HeaderRight extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header/>
+      </div>
+    );
+  }
+}
+export default HeaderRight;
